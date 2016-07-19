f1b57f2dc1ad9f243d01b677c986fabd3dccb4bd
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2016-07-19 12:22:34 -0300

    Alteracao em README e adicionado logo.php e pasta de imagens

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # ImagemBase64
-Entrega uma imagem em base64. Recebendo um parametro json true no GET ele retorna um JSON, senão retorna a imagem.
+Entrega uma imagem em base64.
+
+Recebendo um parametro json true no GET ele retorna um JSON, senão retorna a imagem.
+
+#### Exemplo
+
+url.de.exemplo
+
+####Como Usar
+
+Entre diretamente no arquivo para ver a imagem. Ex:
+
+pasta/do/arquivo/logo.php
+
+Ou faça um GET para receber o json. Ex:
+
+pasta/do/arquivo/logo.php?json=true
+
+Simule um erro de parametro incorreto. Ex:
+
+pasta/do/arquivo/logo.php?json=batatinha
+
+Use diretamente no HTML. Ex:
+
+```html
+<img src='pasta/do/arquivo/logo.php'>
+```
+
+Exemplo usando JS (Usei Jquery para fazer mais rápido, mas use como quiser):
+
+exemplo jquery
---
+++ logo.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Arquivo logo.php
+ * 
+ * Retorna o logotipo em base64 para uma chamada GET em JSON ou Imprime a Imagem
+ * 
+ * Versão do PHP: 7.0 
+ * 
+ * @author Gabriel Bertola Bocca <gabriel at estudiodigitalbocca.com.br>
+ * @copyright (c) 2016, Estúdio Digital Bocca
+ * @version 1.0
+ */
+
+
+/**
+ * Se houver um get json retorna seu valor, senão define $json como false
+ * 
+ * ATENÇÃO: Precisa da versão 7 do PHP, caso esteja usando outra versão troque 
+ * por um IF/ELSE ou qualquer estrutura similar
+ * 
+ * Referência para Null coalescing:
+ * http://php.net/manual/pt_BR/language.operators.comparison.php
+ */
+$json = $_GET['json'] ?? false;
+
+function devolve($json){
+        
+    /**
+     * Transforma letras maiúsculas em minúsculas (Caso seja necessário),
+     * verifica se é uma string com o valor "true" e caso seja,
+     * define um booleano TRUE
+     */
+    if(strtolower($json) == 'true'){
+        $json = true;
+    }
+    
+    /**
+     * Se o valor de $json não for booleano ele dispara uma excessão
+     * com uma mensagem informativa sobre como deve ser feito o pedido.
+     */
+    if(is_bool($json) === false){
+        throw new \Exception("O Parametro 'json' precisa ser TRUE (Case Insensitive). Para receber uma imagem não envie o parametro 'json'");
+    }
+    
+    /**
+     * Lê a imagem que foi definida para este exemplo
+     */
+    $imagem = file_get_contents('img/edb-400.png');
+    
+    /**
+     * Transforma a imagem em texto base64
+     */
+    $encoded = base64_encode($imagem);
+    
+    /**
+     * Verifica se $json é true, monta a resposta e retorna.
+     */
+    if($json === true){
+        $resposta = [
+            "imagem" => $encoded
+        ];
+        
+        header('Content-Type: application/json');
+        echo json_encode($resposta);
+        return;
+    }
+    
+    /**
+     * Monta um cabeçalho e mostra a imagem.
+     */
+    header ('Content-Type: image/png');
+    echo base64_decode($encoded);
+}
+
+/**
+ * Tenta executar a função.
+ */
+try{
+    devolve($json);
+}
+
+/**
+ * Retorna algum erro caso ocorra.
+ */
+catch(\Exception $e){
+    echo $e->getMessage();
+}
